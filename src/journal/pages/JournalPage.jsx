7a76c5c67f35368a0eea70866e1299f6c93fb008
nico1612 <nicogonzalezraggi123@gmail.com>
2023-03-16 11:41:26 -0300

    con error

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,29 +1,48 @@
-import { AddOutlined } from "@mui/icons-material"
-import { IconButton, Typography } from "@mui/material"
-import { JournalLayout } from "../layout/JournalLayout"
-import { NoteView, NothingSelectedView } from "../views"
+import { useDispatch, useSelector } from 'react-redux';
+import { IconButton } from '@mui/material';
+import { AddOutlined } from '@mui/icons-material';
+
+import { JournalLayout } from '../layout/JournalLayout';
+import { NoteView, NothingSelectedView } from '../views';
+import { startNewNote } from '../../store/journal/thunks';
+
+export const JournalPage = () => {
+
+  const {active,isSaving}=useSelector(state=>state.journal)
+
+  const dispatch = useDispatch();
+
+  const onClickNewNote = () => {
+    dispatch( startNewNote() );
+  }
+
+  return (
+    <JournalLayout>
+      
+        {
+            (!!active)
+            ?<NoteView/>
+            :<NothingSelectedView/>
+
+        }
 
 
-export const JournalPage=()=>{
-    return(
-        <JournalLayout>
-            {/*<Typography>qasjdmoeasñwwsdbjjdfusfhrusfhmvdszjk</Typography>*/}
-            <NothingSelectedView/>
-            {/*<NoteView/>*/}
+      <IconButton
+        onClick={ onClickNewNote }
+        size='large'
+        disabled={ isSaving }
+        sx={{
+          color: 'white',
+          backgroundColor: 'error.main',
+          ':hover': { backgroundColor: 'error.main', opacity: 0.9 },
+          position: 'fixed',
+          right: 50,
+          bottom: 50
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30 }} />
+      </IconButton>
 
-            <IconButton
-                size='large'
-                sx={{
-                    color: 'white',
-                    backgroundColor: 'error.main',
-                    ':hover': { backgroundColor: 'error.main', opacity: 0.9 },
-                    position: 'fixed',
-                    right: 50,
-                    bottom: 50
-                }}
-            >
-                <AddOutlined sx={{ fontSize: 30 }} />
-            </IconButton>
-        </JournalLayout>
-    )
+    </JournalLayout>
+  )
 }
---
+++ src/store/journal/journalSlice.js
@@ -0,0 +1,57 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const journalSlice = createSlice({
+    name: 'journal',
+    initialState: {
+        isSaving: false,
+        messageSaved: '',
+        notes: [],
+        active: null,
+        // active: {
+        //     id: 'ABC123',
+        //     title: '',
+        //     body: '',
+        //     date: 1234567,
+        //     imageUrls: [], // https://foto1.jpg, https://foto2.jpg, https://foto3.jpg
+        // }
+    },
+    reducers: {
+        savingNewNote: ( state ) => {
+            state.isSaving = true;
+        },
+        addNewEmptyNote: (state, action ) => {
+            state.notes.push( action.payload );
+            state.isSaving = false;
+        },
+        setActiveNote: (state, action ) => {
+            state.active = action.payload;
+        },
+        setNotes: (state, action ) => {
+        },
+        setSaving: (state ) => {
+        },
+        updateNote: (state, action ) => { // payload: note
+        },
+        setPhotosToActiveNote: (state, action) => {
+        },
+
+        clearNotesLogout: (state) => {
+        },
+
+        deleteNoteById: (state, action ) => {
+        },
+    }
+});
+
+// Action creators are generated for each case reducer function
+export const { 
+    addNewEmptyNote,
+    clearNotesLogout,
+    deleteNoteById, 
+    savingNewNote,
+    setActiveNote,
+    setNotes,
+    setPhotosToActiveNote,
+    setSaving,
+    updateNote,
+} = journalSlice.actions;
